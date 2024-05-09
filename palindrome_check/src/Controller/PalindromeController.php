7b9f225bd778e3dd9b8e9d838dc16f0b6984a809
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-05-09 22:31:12 +0300

    Validate the code

--- palindrome_check/src/Controller/PalindromeController.php
+++ palindrome_check/src/Controller/PalindromeController.php
@@ -4,35 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
-class PalindromeController
- extends AbstractController
+class PalindromeController extends AbstractController
 {
     #[Route('/palindrome', name: 'app_palindrome')]
     public function index(Request $request): Response
     {
-        
-        $string = $request->query->get('string') ?? 'MAM'; 
-        
-        
+        $string = $request->query->get('string');
         $isPalindrome = $this->isPalindrome($string);
 
-       
         return $this->render("palindrome/index.html.twig", [
             'string' => $string,
             'isPalindrome' => $isPalindrome,
         ]);
     }
 
-    
     private function isPalindrome($string)
     {
        
         $cleanedString = strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $string));
-        
-       
         return $cleanedString === strrev($cleanedString);
     }
 }
